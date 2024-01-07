5199e5a439bb842139605c1ee05993def4c1344a
Igor Pcholkin <ipcholkin@gmail.com>
2024-01-07 12:28:24 +0200

    add credit card asterisk anonmymizer

---
+++ src/test/java/com/discreet/datamasking/anonymizer/CreditCardAsteriskAnonymizerTest.java
@@ -0,0 +1,28 @@
+package com.discreet.datamasking.anonymizer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreditCardAsteriskAnonymizerTest {
+    @Test
+    public void testAnonymizeWithDefaultNumberOfTrailingCharsToMask() {
+        CreditCardAsteriskAnonymizer anonymizer = new CreditCardAsteriskAnonymizer();
+
+        String input = "5209761132208795";
+        String output = anonymizer.anonymize(input);
+
+        assertEquals("520976113220****", output);
+    }
+
+    @Test
+    public void testAnonymizeWith10TrailingCharsToMask() {
+        CreditCardAsteriskAnonymizer anonymizer = new CreditCardAsteriskAnonymizer(10);
+
+        String input = "5209761132208795";
+        String output = anonymizer.anonymize(input);
+
+        assertEquals("520976**********", output);
+    }
+
+}
