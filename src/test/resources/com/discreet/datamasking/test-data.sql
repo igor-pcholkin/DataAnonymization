6e69ce5cbf673c9193de1f766c2020e3c94bdf61
Igor Pcholkin <ipcholkin@gmail.com>
2024-01-11 18:23:05 +0200

    add more data to test db

--- src/test/resources/com/discreet/datamasking/test-data.sql
+++ src/test/resources/com/discreet/datamasking/test-data.sql
@@ -1,2 +1,5 @@
-INSERT INTO USERS VALUES(1, 'John Smith');
-INSERT INTO USERS VALUES(2, 'Bradlee Cooper');
+INSERT INTO USERS VALUES(1, 'John Smith', 'john.smith@gmail.com', '92 West Courtland Street Wyandotte, MI 48192', '1986-03-01', '123456-12345', '5432123456789012');
+INSERT INTO USERS VALUES(2, 'Bradlee Cooper', 'bradley.cooper@apple.com', '658 Coffee Dr.Savannah, GA 31404', '1954-09-18', '129856-98765', '4321076587652345');
+
+INSERT INTO COMPANIES VALUES(1, 'Adverse corp', '558 Creekside Road Ridgecrest, CA 93555');
+INSERT INTO COMPANIES VALUES(2, 'Trident corp', '7696 Young St.Severn, MD 21144');
