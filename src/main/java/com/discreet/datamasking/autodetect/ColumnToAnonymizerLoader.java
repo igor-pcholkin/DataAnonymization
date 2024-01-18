c516041afa7ce7a733d54965ad4f9e06d83c1e54
Igor Pcholkin <ipcholkin@gmail.com>
2024-01-18 08:18:52 +0200

    add column to anonymizer loader with test

---
+++ src/main/java/com/discreet/datamasking/autodetect/ColumnToAnonymizerLoader.java
@@ -0,0 +1,23 @@
+package com.discreet.datamasking.autodetect;
+
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+@Component
+public class ColumnToAnonymizerLoader {
+    public Map<String, String> load() {
+        try (InputStream resourceAsStream = this.getClass().getClassLoader()
+                .getResourceAsStream("anonymizerToColumnMapper.properties")) {
+            Properties properties = new Properties();
+            properties.load(resourceAsStream);
+            return new HashMap(properties);
+        } catch (IOException ex) {
+            throw new RuntimeException("Can't load anonymizerToColumnMapper.properties");
+        }
+    }
+}
---
+++ src/test/java/com/discreet/datamasking/autodetect/ColumnToAnonymizerLoaderTest.java
@@ -0,0 +1,27 @@
+package com.discreet.datamasking.autodetect;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ColumnToAnonymizerLoaderTest {
+    ColumnToAnonymizerLoader loader = new ColumnToAnonymizerLoader();
+
+    @Test
+    public void testLoadMappings() {
+        Map<String, String> mappings = loader.load();
+        assertEquals(Map.of(
+                        "name", "name",
+                        "title", "name",
+                        "address", "address",
+                        "ccard", "ccard",
+                        "personal", "pid",
+                        "social_number", "pid",
+                        "taxpayer_number", "pid",
+                        "birthdate", "birthdate"
+                        ),
+                mappings);
+    }
+}
